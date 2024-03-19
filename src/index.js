f751faf24f19231460b475098514ec3065dc4cab
Tikhon Novitskiy <Novitskiy@lanit.ru>
2024-03-19 16:23:24 +0300

    routing template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import SignInComponent from "./components/auth/SignInComponents";
+import SignUpComponent from "./components/auth/SignUpComponents";
+import App from "./App";
+
+const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <App/>,
+    },
+    {
+        path: "/sign-in",
+        element: <SignInComponent/>,
+    },
+    {
+        path: "/sign-up",
+        element: <SignUpComponent/>,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
