5ea0116758d6364b38ccb15ad35936546a6b464b
Tikhon Novitskiy <Novitskiy@lanit.ru>
2024-03-21 18:24:33 +0300

    Auth forms

--- src/components/auth/SignInComponents.jsx
+++ src/components/auth/SignInComponents.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const SignInComponent = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (submitEvent) => {
+        submitEvent.preventDefault();
+        console.log("Username:", username);
+
+        try {
+            const response = await axios.post("http://localhost:8080/auth/sign-in", {
+                "username": username,
+                "password": password
+            });
+            const token = response.data.token;
+            localStorage.setItem("token", token);
+            navigate("/");
+        } catch (error) {
+            setErrorMessage("Something went wrong. Please, try again");
+        }
+    };
+
     return (
         <div>
-            <h1>Sign In Page</h1>
+            <h1>Sign In</h1>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" value={username}
+                               onChange={(changeEvent) => setUsername(changeEvent.target.value)} required/>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" value={password}
+                               onChange={(changeEvent) => setPassword(changeEvent.target.value)} required/>
+                    </div>
+                    <button type="submit">Sign In</button>
+                    <p className="error-msg">{errorMessage}</p>
+                </form>
+            </section>
         </div>
     );
 }
--- src/components/auth/SignUpComponents.jsx
+++ src/components/auth/SignUpComponents.jsx
@@ -1,9 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const SignUpComponent = () => {
-    return (
-        <div>
-            <h1>Sign Up Page</h1>
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (submitEvent) => {
+        submitEvent.preventDefault();
+        console.log("Username:", username);
+
+        try {
+            const response = await axios.post("http://localhost:8080/auth/sign-up", {
+                "username": username,
+                "password": password,
+                "email": email,
+                "phone": phone,
+                "firstName": firstName,
+                "lastName": lastName
+            });
+            const token = response.data.token;
+            localStorage.setItem("token", token);
+            navigate("/");
+        } catch (error) {
+            setErrorMessage(error.response.data);
+        }
+    };
+
+    return (<div>
+            <h1>Sign Up</h1>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" value={username}
+                               onChange={(changeEvent) => setUsername(changeEvent.target.value)} required/>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" value={password}
+                               onChange={(changeEvent) => setPassword(changeEvent.target.value)} required/>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" value={email}
+                               onChange={(changeEvent) => setEmail(changeEvent.target.value)} required/>
+                        <label htmlFor="phone">Phone</label>
+                        <input type="tel" id="phone" value={phone}
+                               onChange={(changeEvent) => setPhone(changeEvent.target.value)} required/>
+                        <label htmlFor="firstName">First name</label>
+                        <input type="text" id="firstName" value={firstName}
+                               onChange={(changeEvent) => setFirstName(changeEvent.target.value)} required/>
+                        <label htmlFor="lastName">Last name</label>
+                        <input type="text" id="lastName" value={lastName}
+                               onChange={(changeEvent) => setLastName(changeEvent.target.value)} required/>
+                    </div>
+                    <button type="submit">Sign Up</button>
+                    <p className="error-msg">{errorMessage}</p>
+                </form>
+            </section>
         </div>
     );
 }
